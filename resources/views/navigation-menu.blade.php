dd6c84e7506318ef86d93eb93e81bad2ac350dce
Artem Darius Weber <mit.3tlasa@gmail.com>
2024-04-13 00:07:03 +0300

    feat: added navigation menu link to museums

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -16,6 +16,12 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link href="{{ route('museum.index') }}" :active="request()->routeIs('museum.index')">
+                        Музеи
+                    </x-nav-link>
+                </div>
             </div>
 
             <div class="hidden sm:flex sm:items-center sm:ms-6">
@@ -141,6 +147,10 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+
+            <x-responsive-nav-link href="{{ route('museum.index') }}" :active="request()->routeIs('museum.index')">
+                Музеи
             </x-responsive-nav-link>
         </div>
 
