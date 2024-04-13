746bea31935f90c89298a16fd1883847206d0d3a
Artem Darius Weber <mit.3tlasa@gmail.com>
2024-04-13 14:29:14 +0300

    feat: created Authors Table component

--- app/Livewire/Author/AuthorsTable.php
+++ app/Livewire/Author/AuthorsTable.php
@@ -2,12 +2,18 @@
 
 namespace App\Livewire\Author;
 
+use App\Models\Author;
+use Livewire\Attributes\On;
 use Livewire\Component;
 
 class AuthorsTable extends Component
 {
+    public $search = "";
+
+    #[On('authorAdded')]
     public function render()
     {
-        return view('livewire.author.authors-table');
+        $authors = Author::OrderBy('created_at', 'desc')->paginate(10);
+        return view('livewire.author.authors-table', compact('authors'));
     }
 }
