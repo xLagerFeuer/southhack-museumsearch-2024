d4e4a92e81cf1b11ff351569b508693778f0206f
Artem Darius Weber <mit.3tlasa@gmail.com>
2024-04-13 14:24:31 +0300

    feat: created CreateAuthorForm component

--- app/Livewire/Author/AuthorCreateForm.php
+++ app/Livewire/Author/AuthorCreateForm.php
@@ -2,10 +2,24 @@
 
 namespace App\Livewire\Author;
 
+use App\Models\Author;
 use Livewire\Component;
 
 class AuthorCreateForm extends Component
 {
+
+    public $name;
+
+    public function createMuseum()
+    {
+        $this->validate([ 'name' => 'required|min:4|max:100' ]);
+
+        Author::create([ 'name' => $this->name ]);
+
+        $this->reset();
+
+        $this->dispatch('authorAdded');
+    }
     public function render()
     {
         return view('livewire.author.author-create-form');
--- resources/views/livewire/author/author-create-form.blade.php
+++ resources/views/livewire/author/author-create-form.blade.php
@@ -1,3 +1,34 @@
-<div>
-    {{-- Nothing in the world is as soft and yielding as water. --}}
+<div class="bg-white rounded-lg shadow sm:max-w-md sm:w-full sm:mx-auto sm:overflow-hidden">
+    <div class="px-4 py-8 sm:px-10">
+        <div class="relative mt-6">
+            <div class="absolute inset-0 flex items-center">
+                <div class="w-full border-t border-gray-300"></div>
+            </div>
+            <div class="relative flex justify-center text-sm leading-5">
+                <span class="px-2 text-gray-500 bg-white">
+                    Создать автора
+                </span>
+            </div>
+        </div>
+        <div class="mt-6">
+            <form wire:submit.prevent="createMuseum">
+                <div class="w-full space-y-6">
+                    <div class="w-full">
+                        <div class="relative">
+                            <input wire:model="name" required type="text" class="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Введите имя автора"/>
+                            @error('name') <span class="text-red-500">{{ $message }}</span> @enderror
+                        </div>
+                    </div>
+
+                    <div>
+                        <span class="block w-full rounded-md shadow-sm">
+                            <button type="submit" class="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg">
+                                Создать
+                            </button>
+                        </span>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
 </div>
